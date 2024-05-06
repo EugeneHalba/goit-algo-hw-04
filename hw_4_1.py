872744ccfaa36b697ac504fbebda5c192794eef4
EugeneHalba <161532538+EugeneHalba@users.noreply.github.com>
2024-05-06 07:45:31 +0300

    Add files via upload

--- hw_4_1.py
+++ hw_4_1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-def total_salary(path):
-    path = Path('text1.txt')
-    if path.exists():
+path = Path('.')
+try:
+    def total_salary(path):
         with open(path, 'r', encoding="utf-8") as file:
             salary = list()
             for line in file:        
@@ -9,26 +9,17 @@ def total_salary(path):
             new_salary = salary
             new_salary = [line.rstrip() for line in new_salary] 
             new_salary_len = len(new_salary)
-
-            total = 0
- 
-            for line in new_salary:
-            
+            total = 0  
+            for line in new_salary:               
                 salary_int = int(line)
                 total += salary_int      
                 average = total // new_salary_len
-                total_salary = (total,average)
-            return (total_salary) 
-    else:
-        print (f'Файл {path} відсутній')
-        exit ()   
-           
-    
-total,average = total_salary('my_repo')     
+                total_salary = (total,average) 
+                  
+            return (total_salary)
+
+except Exception as e:
+    print(f'{e} ')
+   
+total,average = total_salary('text.txt')     
 print (f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-
-    
-    
-    
-    
-
